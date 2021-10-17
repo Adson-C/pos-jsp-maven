eb2f2a268de9e32a17776a06f8b346b280957171
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-10-17 13:11:05 -0300

    Criando versionador de Bancos de dados

--- santos-jsp/src/main/java/filter/FilterAutenticao.java
+++ santos-jsp/src/main/java/filter/FilterAutenticao.java
@@ -1,10 +1,14 @@
 package filter;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import connection.SingleConnectionBanco;
+import dao.DaoVersionadorBanco;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -86,7 +90,48 @@ public class FilterAutenticao implements Filter {
 	public void init(FilterConfig fConfig) throws ServletException {
 		connection = SingleConnectionBanco.getConnection();
 		
+		DaoVersionadorBanco daoVersionadorBanco = new DaoVersionadorBanco();
 		
-	}
+		String caminhoPastaSql = fConfig.getServletContext().getRealPath("versionadorbancosql") +  File.separator;
+		
+		File[] filesSql = new File(caminhoPastaSql).listFiles();
+		
+		try {
+			for (File file : filesSql) {
+				
+				boolean arquivoJaRodado = daoVersionadorBanco.arquivoSqlRodado(file.getName());
+				
+				if (!arquivoJaRodado) {
+					
+					FileInputStream entradaArquivo = new FileInputStream(file);
+					
+					Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
+					
+					StringBuilder sql = new StringBuilder();
+					
+					while (lerArquivo.hasNext()) {
+						
+						sql.append(lerArquivo.nextLine());
+						sql.append("\n");
+					}
+					
+					connection.prepareStatement(sql.toString()).execute();
+					daoVersionadorBanco.gravaArquivoRodado(file.getName());
+					
+					connection.commit();
+					lerArquivo.close();
+				}
+			}
+			
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		}
+		
 
 }
